6d42d90142866c39e541e20d71e7eb59894121b9
Shania Saliba <shania.saliba.23@um.edu.mt>
2025-03-02 12:18:01 +0100

    Resolved conflicts

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -28,6 +28,7 @@ function Booking({ rooms }) {
       <div className="header-container">
         <h1 className="header">The Opulence Hotel</h1>
         <p className="general-text">
+        <p className="general-text">
           From <b>15th June 2025</b> to <b>21st June 2025</b> -{" "}
           <b>2 adults, 1 child</b>
         </p>
