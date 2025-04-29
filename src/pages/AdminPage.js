5550821fa4697b336d63e314d9bdd84fae1ef866
Julia Curmi <curmijulia@gmail.com>
2025-04-29 19:16:35 +0200

    fixed booking page

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import ".//Admin.css";
+import "../Admin.css";
 import {
   BarChart,
   Bar,
